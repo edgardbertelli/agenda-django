ddedf241c69ff05e6590575b8c3266bdc79e409d
Edgard Bertelli <bertelliedgard@gmail.com>
2020-07-14 16:23:27 -0300

    Consultando banco de dados e refatorando layout

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from core.models import Evento
+
 
 # Create your views here.
+
+def lista_eventos(request):
+    eventos = Evento.objects.all()
+    dados = {'eventos': eventos}
+    return render(request, 'agenda.html', dados)
